c4a61b926cfee531bb4e8a44969922dfab42530e
nonsensery <ajohnson@quickleft.com>
2015-10-04 15:59:12 -0700

    Add uglify and sourcemaps to build

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
 
 gulp.task('default', function() {
   gulp.src('src/*.js')
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('dist'));
 });
