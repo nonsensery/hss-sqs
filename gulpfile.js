dc4989c0bca0e1318ba0ccb2a3d8c429b1464bb1
nonsensery <ajohnson@quickleft.com>
2015-10-04 16:46:37 -0700

    Add Babel to the JS build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 var gulp = require('gulp');
 var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
+var babel = require('gulp-babel');
+var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 
 gulp.task('js', function () {
   gulp.src('src/*.js')
     .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(concat('product-page-custom.js'))
     .pipe(uglify())
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('dist'));
