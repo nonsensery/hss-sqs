6764259dafea4243c8752ae847744b73e31c0e78
nonsensery <ajohnson@quickleft.com>
2015-10-04 16:29:26 -0700

    Add custom styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,22 @@
 var gulp = require('gulp');
 var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
 
-gulp.task('default', function() {
+gulp.task('js', function () {
   gulp.src('src/*.js')
-    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(sourcemaps.init())
     .pipe(uglify())
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('dist'));
 });
+
+gulp.task('css', function () {
+  gulp.src('src/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['js', 'css']);
