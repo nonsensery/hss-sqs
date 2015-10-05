e0c43b6681581a17d97cc3c7ceb23c01324d5109
nonsensery <ajohnson@quickleft.com>
2015-10-04 22:29:02 -0700

    (Minor) code clean up

--- src/product-page-custom.js
+++ src/product-page-custom.js
@@ -1,18 +1,25 @@
 (function(win,doc){
 
-const ADD_TO_CART_BUTTON_TEXT = 'Choose Details…';
-
 // Emergency exit:
-if (!supported()) {
+if (!isSupportedBrowser()) {
   return;
 }
 
 /**
  Whether this browser is supported
  */
-function supported() {
-  return !!(doc.querySelectorAll && doc.addEventListener);
+function isSupportedBrowser() {
+  return !!(doc.querySelectorAll && doc.addEventListener && [].filter && [].forEach);
 }
+
+const PRODUCT_EXCERPT_SEL = '.product-excerpt';
+const PRODUCT_GALLERY_SEL = '#productGallery';
+const PRODUCT_PRICE_SEL = '.product-price';
+const PRODUCT_QUANTITY_SEL = '.product-quantity-input';
+const PRODUCT_FORM_SEL = '.sqs-async-form-content';
+const ADD_TO_CART_BUTTON_SEL = '.sqs-add-to-cart-button';
+const ADD_TO_CART_INNER_SEL = '.sqs-add-to-cart-button-inner';
+const ADD_TO_CART_HTML = 'Choose Details…';
 
 /**
  jQuery-like alias for querySelectorAll.
@@ -22,15 +29,14 @@ function $(selector, root) {
 }
 
 /**
- Initializes this stuff.
+ Appends the HTML under the parent node.
  */
-function start() {
-  productPageLoaded();
+function appendHTML(parentNode, html) {
+  var div = doc.createElement('div');
+  div.innerHTML = html;
 
-  var addToCartBtn = $('.sqs-add-to-cart-button')[0];
-
-  if (addToCartBtn) {
-    addToCartBtn.addEventListener('click', addToCartClicked);
+  while (div.firstChild) {
+    parentNode.appendChild(div.firstChild);
   }
 }
 
@@ -38,24 +44,30 @@ function start() {
  Callback for when the product page loads.
  */
 function productPageLoaded() {
-  var excerpt = $('.product-excerpt')[0];
-  var gallery = $('#productGallery')[0];
+  // Move product excerpt to the left column:
+
+  var excerpt = $(PRODUCT_EXCERPT_SEL)[0];
+  var gallery = $(PRODUCT_GALLERY_SEL)[0];
 
   if (excerpt && gallery) {
     gallery.appendChild(excerpt);
   }
 
-  var price = $('.product-price')[0];
-  var quantity = $('.product-quantity-input')[0];
+  // Move price below the variant selectors:
+
+  var price = $(PRODUCT_PRICE_SEL)[0];
+  var quantity = $(PRODUCT_QUANTITY_SEL)[0];
 
   if (price && quantity) {
     quantity.parentNode.insertBefore(price, quantity);
   }
 
-  var addToCart = $('.sqs-add-to-cart-button-inner')[0];
+  // Change text of the "Add to Cart" button:
+
+  var addToCart = $(ADD_TO_CART_INNER_SEL)[0];
 
   if (addToCart) {
-    addToCart.innerHTML = ADD_TO_CART_BUTTON_TEXT;
+    addToCart.innerHTML = ADD_TO_CART_HTML;
   }
 }
 
@@ -63,17 +75,31 @@ function productPageLoaded() {
  Callback for when the "Add to Cart" button is clicked.
  */
 function addToCartClicked() {
-  setTimeout(productFormLoaded, 200);
+  setTimeout(function () {
+    if ($(PRODUCT_FORM_SEL)[0]) {
+      productFormLoaded();
+    }
+  }, 500);
 }
 
 /**
  Callback for when the Product Form is loaded.
  */
 function productFormLoaded() {
-  var form = $('.sqs-async-form-content');
+  var form = $(PRODUCT_FORM_SEL)[0];
 }
 
-// Start it up!
-doc.addEventListener('DOMContentLoaded', start);
+/**
+ Initialize this stuff.
+ */
+doc.addEventListener('DOMContentLoaded', function () {
+  productPageLoaded();
+
+  var addToCartBtn = $(ADD_TO_CART_BUTTON_SEL)[0];
+
+  if (addToCartBtn) {
+    addToCartBtn.addEventListener('click', addToCartClicked);
+  }
+});
 
 })(window,document);
