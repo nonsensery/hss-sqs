34f367722d150ace5f658fb8eb8d0be81b43f6df
nonsensery <ajohnson@quickleft.com>
2015-10-25 15:14:51 -0700

    Add `assemble` task to assemble the final HTML fragment

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var babel = require('gulp-babel');
 var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 
+var fs = require('fs');
+
 gulp.task('js', function () {
   gulp.src('src/*.js')
     .pipe(sourcemaps.init())
@@ -21,6 +23,18 @@ gulp.task('css', function () {
     .pipe(sass())
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('dist'));
+});
+
+gulp.task('assemble', function () {
+  var parts = [
+    '<script>\n',
+    fs.readFileSync('dist/product-page-custom.js'),
+    '</script><style type="text/css">\n',
+    fs.readFileSync('dist/product-page-custom.css'),
+    '</style>\n'
+  ];
+
+  fs.writeFileSync('dist/final.html', parts.join(''));
 });
 
 gulp.task('default', ['js', 'css']);
